b41a8c9a6eab25fc1243a5af9447322fc531163a
ljakimczuk <39192420+ljakimczuk@users.noreply.github.com>
2020-09-07 20:00:00 +0200

    Update terraform.tf

--- test/terraform.tf
+++ test/terraform.tf
@@ -1,5 +1,13 @@
 provider "aws" {
-  region 	= "eu-west-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id = true
+  skip_metadata_api_check = true
+
+  s3_force_path_style = true
+
+  endpoints {
+    s3 = "http://localstack:4566"
+  }
 }
 
 module "s3_bucket" {
