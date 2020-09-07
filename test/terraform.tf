ca50f66dabc3ef20b444c3ce32499b6de4f83024
ljakimczuk <39192420+ljakimczuk@users.noreply.github.com>
2020-09-07 20:09:10 +0200

    Update terraform.tf

--- test/terraform.tf
+++ test/terraform.tf
@@ -1,5 +1,9 @@
 provider "aws" {
   region                      = "eu-west-1"
+  
+  access_key                  = "fake-access-key"
+  secret_key                  = "fake-secret-key"
+  
   skip_credentials_validation = true
   skip_requesting_account_id  = true
   skip_metadata_api_check     = true
